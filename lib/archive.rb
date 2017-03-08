157f7142eb48d5c65392db24e50caee7ad20426d
Takuma YOSHIOKA <tashioka.256@gmail.com>
2017-03-09 02:52:44 +0900

    Improve monthly and yearly archives
    - Add next / prev links.
    - Rewrite helpers.
      * Prefer data stream to loop and side effect.

--- lib/archive.rb
+++ lib/archive.rb
@@ -3,66 +3,146 @@ module Larry
     include Nanoc::Helpers::HTMLEscape
     ARCHIVE_LIST_CAPACITY = 15
 
-    def articles_by_year_month
+    # Descending order of year.
+    # [(year, (article_first_index, article_last_index)]
+    # (currently unused.)
+    def article_years_indices
       blk = -> {
-        result = {}
-        current_year = nil
-        current_month = nil
-        current_year_items = nil
-        current_month_items = nil
-
-        sorted_articles.each do |article|
-          created_at = article[:created_at]
-          if current_year != created_at.year
-            current_year = created_at.year
-            current_year_items = result[current_year] = {}
-            current_month = created_at.month
-            current_month_items = current_year_items[current_month] = []
-          elsif current_month != created_at.month
-            current_month = created_at.month
-            current_month_items = current_year_items[current_month] = []
-          end
-          current_month_items << article
-        end
-        result
+        sorted_articles.map.with_index {|item, i| [item, i]} # [(item, index)]
+        group_by {|it_i| it_i[0][:created_at].year}.to_a.sort.reverse. # [(year, [(item, index)])]
+          map {|y_its_is|
+            minmax = y_its_is[1].map {|it_i| it_i[1]}.minmax
+            [y_its_is[0], minmax]
+          } # [(year, (first_index, last_index))]
       }
-
       if @items.frozen?
-        @article_items_by_year_month ||= blk.call
+        @article_years_indices ||= blk.call
+      else
+        blk.call
+      end
+    end
+
+    # Descending order.
+    # [year]
+    def article_years
+      blk = -> {
+        articles.map {|item| item[:created_at].year}.uniq.sort.reverse
+      }
+      if @items.frozen?
+        @article_years ||= blk.call
+      else
+        blk.call
+      end
+    end
+
+    # Descending order of year & month.
+    # [(year, month), (first_index, last_index))]
+    def article_years_and_months_indices
+      blk = -> {
+        sorted_articles.map.with_index {|item, i| [item, i]}. # [(item, index)]
+          group_by {|it_i|
+            created_at = it_i[0][:created_at]
+            [created_at.year, created_at.month]
+          }. # [((year, month), [(item, index)])]
+          map {|ym_its_is|
+            minmax = ym_its_is[1].map {|it_i| it_i[1]}.minmax
+            [ym_its_is[0], minmax]
+          }. # [((year, month), (first_index, last_index))]
+          sort_by {|ym_fl| ym_fl[0]}.reverse # Descending order of year & month.
+      }
+      if @items.frozen?
+        @article_years_and_months_indices ||= blk.call
+      else
+        blk.call
+      end
+    end
+
+    # Descending order of year & month.
+    # [(year, month)]
+    def article_years_and_months
+      blk = -> {
+        articles.map {|item| [item[:created_at].year, item[:created_at].month]}.uniq.sort.reverse
+      }
+      if @items.frozen?
+        @article_years_and_months ||= blk.call
       else
         blk.call
       end
     end
 
     def create_archive_pages
-      archives_root = '/archives'
-      articles_by_year_month.each do |year, year_items|
+      # years: [year]
+      years = article_years
+      years_months = article_years_and_months
+      # ym_fl_arr: [((year, month), (first_i, last_i))]
+      ym_fl_arr = article_years_and_months_indices
+      # y_mifls_arr: [(year, [month, ym_index, (first_i, last_i)])]
+      y_mifls_arr = ym_fl_arr.
+        map.with_index {|ym_fl, i| [ym_fl[0], i, ym_fl[1]]}. # [((year, month), ym_index, (first_i, last_i))]
+        group_by {|ym_i_fl| ym_i_fl[0][0]}.to_a.sort.reverse. # [(year, [((year, month), ym_index, (first_i, last_i))])]
+        map {|y_ymifls| [y_ymifls[0], y_ymifls[1].map {|ym_i_fl|
+          [ym_i_fl[0][1], ym_i_fl[1], ym_i_fl[2]]
+        }]} # [(year, [(month, ym_index, (first_i, last_i))])]
+      y_mifls_arr.each_with_index do |y_mifls, year_index|
+        year = y_mifls[0]
+        # m_i_fl_arr: [(month, ym_index, (first_i, last_i))]
+        m_i_fl_arr = y_mifls[1]
+        next_year = years[year_index - 1] if year_index > 0
+        prev_year = years[year_index + 1] if year_index < years.size - 1
         year4 = sprintf('%04d', year)
-        page_path = archives_root + "/#{year4}/index.html"
+        next_year4 = sprintf('%04d', next_year) if next_year
+        prev_year4 = sprintf('%04d', prev_year) if prev_year
+        # months: [(month, month2, (first_i, last_i))]
+        months = m_i_fl_arr.map {|m_i_fl|
+          [m_i_fl[0], sprintf('%02d', m_i_fl[0]), m_i_fl[2]]
+        }
+
+        # Create an yearly archive.
         @items.create(
           # content
           '',
           # attributes
           {
             title: "Archive: #{year4}",
-            year: year,
+            year4: year4,
+            next_year4: next_year4,
+            prev_year4: prev_year4,
+            months: months,
           },
           # path
-          page_path)
-        year_items.each do |month, month_items|
+          "/#{year4}/index.html")
+
+        # Create monthly archives.
+        m_i_fl_arr.each do |m_i_fl|
+          month = m_i_fl[0]
+          ym_index = m_i_fl[1]
+          first_index = m_i_fl[2][0]
+          last_index = m_i_fl[2][1]
+          next_ym = years_months[ym_index - 1] if ym_index > 0
+          prev_ym = years_months[ym_index + 1] if ym_index < years_months.size - 1
           month2 = sprintf('%02d', month)
-          page_path = archives_root + "/#{year4}/#{month2}/index.html"
+          next_ym_year4 = sprintf('%04d', next_ym[0]) if next_ym
+          next_ym_month2 = sprintf('%02d', next_ym[1]) if next_ym
+          prev_ym_year4 = sprintf('%04d', prev_ym[0]) if prev_ym
+          prev_ym_month2 = sprintf('%02d', prev_ym[1]) if prev_ym
           @items.create(
             # content
             '',
             # attributes
             {
-              title: "Archive: #{year4}/#{month2}",
-              year: year,
+              title: "Archive: #{year4}",
+              year4: year4,
               month: month,
+              month2: month2,
+              next_ym_year4: next_ym_year4,
+              next_ym_month2: next_ym_month2,
+              prev_ym_year4: prev_ym_year4,
+              prev_ym_month2: prev_ym_month2,
+              first_index: first_index,
+              last_index: last_index,
             },
             # path
-            page_path)
+            "/#{year4}/#{month2}/index.html")
         end
       end
     end
