2eb4c649aaf59125be767600883556066dd6ef04
Takuma YOSHIOKA <tashioka.256@gmail.com>
2017-03-12 21:00:25 +0900

    Rewrite `xhtml5ize` filter using SAX XML parser
    Previous implementation does XML parsing twice at most but it was not
    desirable.
    Now the parsing will be done only once for a document.
    
    Previous implementation find and omit some close tags by regex, but it
    doesn't check they are really tags (or others like cdata or comment).
    Now the close tags are correctly omitted.

--- lib/xhtml5ize.rb
+++ lib/xhtml5ize.rb
@@ -1,68 +1,173 @@
+require 'nokogiri'
 module Larry
+  class Xhtml5Doc < ::Nokogiri::XML::SAX::Document
+    attr_reader :content
+
+    HTML_NS = 'http://www.w3.org/1999/xhtml'
+
+    def initialize(html_mode=false)
+      @content = "<!DOCTYPE html>\n".dup
+      @is_prev_tag_omissible = false
+      @is_html_mode = html_mode
+    end
+
+    def update_omissibility(omissibility=false)
+      if @is_prev_tag_omissible
+        if !omissibility
+          @content << '>'
+        end
+        @is_prev_tag_omissible = false
+      end
+    end
+
+    def is_html_elem(uri)
+      if @is_html_mode
+        # HTML namespace would be omitted.
+        uri.nil?
+      else
+        # HTML namespace might (or might not) be omitted.
+        uri.nil? || uri == HTML_NS
+      end
+    end
+
+    def is_close_tag_omissible(name, uri)
+      if is_html_elem(uri)
+        case name.downcase
+        when 'meta', 'link', 'br', 'hr'
+          # HTML has some tags whose close tag should be omitted.
+          true
+        else
+          # Basically HTML elements (like `a`, `i`, `script`, etc...)
+          # are not allowed to omit close tag.
+          false
+        end
+      else
+        # Always true for non-HTML elements.
+        true
+      end
+    end
+
+    def escape_text(text)
+      text.
+        gsub(/&/, '&amp;').
+        gsub(/</, '&lt;').
+        gsub(/>/, '&gt;')
+    end
+
+    def escape_attr(text)
+      text.
+        gsub(/&/, '&amp;').
+        gsub(/</, '&lt;').
+        gsub(/>/, '&gt;').
+        gsub(/"/, '&quot;').
+        gsub(/'/, '&apos;')
+    end
+
+    # HTML parser calls this.
+    def start_element(name, attrs=[])
+      start_element_namespace(
+        name,
+        attrs.map {|name, value|
+          ::Nokogiri::XML::SAX::Parser::Attribute.new(name, nil, nil, value)
+        })
+    end
+
+    def start_element_namespace(name, attrs=[], prefix=nil, uri=nil, ns=[])
+      if name.downcase == 'meta' && is_html_elem(uri)
+        if attrs.any? {|attr| attr.localname == 'http-equiv' && attr.value.downcase == 'content-type' }
+          # This node should be omitted.
+          return
+        end
+      end
+      update_omissibility
+      @is_prev_tag_omissible = true
+      @content << '<'
+      if prefix
+        @content << prefix << ':'
+      end
+      @content << name
+
+      attrs.each do |attr|
+        @content << ' '
+        if attr.prefix
+          @content << attr.prefix << ':'
+        end
+        @content << attr.localname << '="' << escape_attr(attr.value) << '"'
+      end
+    end
+
+    # HTML parser calls this.
+    def end_element(name)
+      end_element_namespace(name)
+    end
+
+    def end_element_namespace(name, prefix=nil, uri=nil)
+      if @is_prev_tag_omissible
+        if is_close_tag_omissible(name, uri)
+          @content << ' />'
+        else
+          @content << "></#{name}>"
+        end
+      else
+        @content << "</#{name}>"
+      end
+      @is_prev_tag_omissible = false
+    end
+
+    def cdata_block(text)
+      update_omissibility
+      if @is_html_mode
+        # Content inside `script` tag is treated as CDATA by
+        # HTML parser, and the content would be already escaped.
+        @content << text
+      else
+        @content << escape_text(text)
+      end
+    end
+
+    def characters(text)
+      update_omissibility
+      @content << escape_text(text)
+    end
+
+    def comment(text)
+      update_omissibility
+      @content << '<!-- ' << text << '-->'
+    end
+
+    def processing_instruction(name, content)
+      # If `name.downcase == 'xml'`, it may be XML decleration.
+      # Ignore it.
+      if name.downcase != 'xml'
+        update_omissibility
+        @content << '<?' << name << ' ' << content << '?>'
+      end
+    end
+  end
+
   class Xhtml5ize < Nanoc::Filter
     identifier :xhtml5ize
     requires 'nokogiri'
 
     def run(content, params = {})
       Nanoc::Extra::JRubyNokogiriWarner.check_and_warn
-      #cond_html_elem = '[namespace-uri()="http://www.w3.org/1999/xhtml"]'
-      cond_html_elem = '[namespace-uri()="http://www.w3.org/1999/xhtml" or namespace-uri()=""]'
 
       in_type = params.fetch(:type, :html)
+      #out = ::Nokogiri::XML::SAX::PushParser.new(Xhtml5Doc, nil, encoding = 'UTF-8')
       case in_type
       when :html
         # Write as XHTML because `::Nokogiri::HTML#to_xml` doesn't close `<meta>` tag...
         # Note that `to_xhtml` wraps some texts with CDATA section, which is not available for html5.
-        xml_content = ::Nokogiri::HTML(content, nil, 'utf-8').to_xhtml
+        xhtml5doc = Xhtml5Doc.new(true)
+        parser = ::Nokogiri::HTML::SAX::Parser.new(xhtml5doc)
       when :xml, :xhtml
-        xml_content = content
+        xhtml5doc = Xhtml5Doc.new(false)
+        parser = ::Nokogiri::XML::SAX::Parser.new(xhtml5doc)
       else
         raise "unknown syntax: #{type.inspect} (expected :html, :xhtml or :xml)"
       end
-      xml_doc = ::Nokogiri::XML(xml_content, nil, 'utf-8')
-
-      # Remove `<meta http-equiv="Content-Type" ... />` element.
-      meta_content_type_selector = "//*[local-name()='meta']#{cond_html_elem}[@http-equiv]"
-      xml_doc.xpath(meta_content_type_selector).each do |node|
-        if node.attribute('http-equiv').value.downcase == 'content-type'
-          # Remove previous blank text node if exists.
-          prev_node = node.previous
-          if prev_node && prev_node.text? && prev_node.blank?
-            prev_node.remove
-          end
-          # Remove `meta` element.
-          node.remove
-        end
-      end
-
-      # Insert `//\n` to empty `script` element to prevent putting Nokogiri to output `<script ... />`.
-      # (HTML5 does not allow `script` elements to omit close tags.
-      # `Nokogiri::XML::Node::SaveOptions::NO_EMPTY_TAGS` also prevents this.
-      #empty_script_selector = "//*[local-name()='script']#{cond_html_elem}[not(node())]"
-      #xml_doc.xpath(empty_script_selector).each do |node|
-      #  # `script` with `@src` can contain JS comments.
-      #  # See https://www.w3.org/TR/2014/REC-html5-20141028/scripting-1.html#script-documentation .
-      #  node.content = "//\n"
-      #end
-
-      # Replace CDATA section with (escaped) plain text.
-      replace_cdata(xml_doc)
-
-      #xml_doc.to_xml(save_with: ::Nokogiri::XML::Node::SaveOptions::NO_DECLARATION)
-      serialized_doc = xml_doc.to_xml(save_with: ::Nokogiri::XML::Node::SaveOptions::new.no_declaration.no_empty_tags)
-
-      # Omit close tags which are not allowed in HTML5.
-      serialized_doc.gsub(/><\/(?:meta|link|br|hr)>/, ' />')
-    end
-
-    def replace_cdata(node, doc=node)
-      node.children.each do |child|
-        if child.cdata?
-          child.replace(doc.create_text_node(child.content))
-        elsif child.element?
-          replace_cdata(child, doc)
-        end
-      end
+      parser.parse(content)
+      xhtml5doc.content
     end
   end
 end
